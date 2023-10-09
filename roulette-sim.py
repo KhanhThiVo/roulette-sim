4a328762b5ee75da0e4852bf63e18e365b2c851b
Thi Vo <khanhthivo97@gmail.com>
2023-10-09 19:34:20 +0200

    Minor change

--- roulette-sim.py
+++ roulette-sim.py
@@ -6,6 +6,7 @@ COND = {
     'double_zero': '0-28-9-26-30-11-7-20-32-17-5-22-34-15-3-24-36-13-1-00-27-10-25-29-12-8-19-31-18-6-21-33-16-4-23-35-14-2',
 }
 
+# blablablabla
 
 def sim_roulette(cond_seq: str, green_on: bool = False):
     sim_range = get_cond(cond_seq)
