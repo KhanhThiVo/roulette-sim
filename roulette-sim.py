ad9486270b16da889a9647ebcd471e736257c9f0
Thi Vo <khanhthivo97@gmail.com>
2023-10-09 20:23:06 +0200

    Minor Progress

--- roulette-sim.py
+++ roulette-sim.py
@@ -6,7 +6,20 @@ COND = {
     'double_zero': '0-28-9-26-30-11-7-20-32-17-5-22-34-15-3-24-36-13-1-00-27-10-25-29-12-8-19-31-18-6-21-33-16-4-23-35-14-2',
 }
 
-# blablablabla
+
+class RouletteAgent:
+
+    def __init__(self, init_budget):
+        self.budget = init_budget
+
+    def bet(self):
+        # TODO: implement betting strategy
+        return None
+
+    def observe(self):
+        # TODO: brainstorm / implement
+        return None
+
 
 def sim_roulette(cond_seq: str, green_on: bool = False):
     sim_range = get_cond(cond_seq)
@@ -18,10 +31,6 @@ def sim_roulette(cond_seq: str, green_on: bool = False):
         outcome = sim_range[np.random.randint(0, 38)]
         if outcome % 2 == 1:
             hits += 1
-
-
-def sim_agent():
-    return None
 
 
 def run():
